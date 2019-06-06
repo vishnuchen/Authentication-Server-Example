fd6fc7204bc2d6d3d64ca51848931ddabe0bfd97
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-06 15:16:35 +0000

    Server with lang preference and later authentication implimented

---
+++ langServer.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const parser = require("cookie-parser");
+const app = express();
+const PORT = process.env.PORT || 8000; // default port 8000
+
+let preference = null;
+
+app.use(parser('I am el zorro'))
+
+app.use(function(req, res, next) {
+  console.log(req.url, req.headers.cookie)
+  console.log(req.cookies)
+  console.log(req.signedCookies)
+  next()
+})
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  if (req.signedCookies.preference == 'EN') {
+    res.render("english")
+  } else if (req.signedCookies.preference == 'FR') {
+    res.render("french")
+  } else {
+    res.render("choose_language");  
+  }
+});
+
+app.get("/choose_english", (req, res) => {
+  res.cookie('preference', 'EN', {signed: true})
+  res.render("english");
+})
+
+app.get("/choose_french", (req, res) => {
+  res.cookie('preference', 'FR', {signed: true})
+  res.render("french");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
---
+++ userAuthenExample.js
@@ -0,0 +1,94 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+const PORT = process.env.PORT || 8000; // default port 8000
+
+app.use(express.static('public'))
+
+// parse application/x-www-form-urlencoded form data into req.body
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('The chipmunk made a lot of noise'));
+
+app.set("view engine", "ejs")
+
+const data = {
+  users: [
+    { username: 'monica', password: 'testing'},
+    { username: 'khurram', password: '12345' }
+  ]
+}
+
+function validUser (username) {
+  for (user of data.users) {
+    if (user.username === username ) {
+      return user;
+    }
+  }  
+}
+
+function checkLogin(username, password) {
+  for (user of data.users) {
+    if (user.username === username && password === user.password) {
+      return user;
+    }
+  }
+}
+
+// Show the treasure if they are logged in, otherwise redirect to LOGIN page.
+app.get("/", (req, res) => {
+  const currentUsername = req.cookies['username'];
+  if (validUser(currentUsername)) {
+    res.render('treasure', { currentUser: currentUsername });
+  } else {
+    res.redirect("login");
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.cookie('username', '')
+  res.render("login")
+})
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = checkLogin(username, password);
+
+  if (user) {
+    // success
+    // cookies set to expire in 1 hour
+    res.cookie('username', user.username, {expires: new Date(Date.now() + 1000*60*60)}); // Set-Cookie: lang=en
+    // res.cookie('password', user.password, {expires: new Date(Date.now() + 1000*60*60)}); // Set-Cookie: lang=en
+
+    res.redirect('/');
+  } else {
+    // failed attempt
+    res.render('login', { errorFeedback: 'Failed to find a user.' });
+  }
+  console.log(`You attempted to log in with ${username}. User: ${user && user.username}`);
+})
+
+app.get("/signup", (req, res) => {
+  res.render("signup")
+})
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  data.users.push({username: username, password: password});
+  res.cookie('username', username, {expires: new Date(Date.now() + 1000*60*60)}); 
+  res.redirect('/');
+})
+
+app.get("/logout", (req, res) => {
+  res.redirect("/login")
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
